cace1a481cad5472db2593ff8fcae3c57720f9cd
Hunter Wilhelm <zicy@zicy.net>
2021-10-09 19:24:09 -0600

    Co-authored-by: Reece Poulsen <Reecepoulsen@users.noreply.github.com> Co-authored-by: Fernelius, Benjamin <benjifern@byui.edu>

--- src/index.js
+++ src/index.js
@@ -6,6 +6,14 @@ const path = require('path');
 const app = express()
 const expressWs = require('express-ws')(app);
 
+// Set up firebase
+const admin = require('firebase-admin');
+const serviceAccount = require('../firebaseSDKinfo.json');
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+const db = admin.firestore();
+
 const PORT = 3000
 // instructions
 // http://localhost:3000/
@@ -16,23 +24,44 @@ const PORT = 3000
 const my_client_id = '5dd2dda670184f4688b9eef425c31877'; // Symphony's permanent ID
 const redirectURL = encodeURIComponent("http://localhost:3000/callback");
 
-async function getSpotifyInfo(api, sendMessage) {
-  sendMessage(JSON.stringify({state: "user-info"}));
+// downloads the information from spotify api and send status updates to browser
+async function getSpotifyInfo(api, messageCallback) {
+  const codes = LOADING_CODE;
+
+  messageCallback(codes.USER_INFO);
   const user = await getUserInfo(api);
-  if (!user) return;
+  if (!user) throw 'Invalid User Token';
   const username = user.id;
-  sendMessage(JSON.stringify({state: "user-playlists"}));
+
+  messageCallback(codes.USER_PLAYLISTS);
   const playlists = await getUserPlaylists(api, username);
   const userPlaylists = playlists.filter(playlist => username === playlist.owner.id);
-  sendMessage(JSON.stringify({state: "user-saved-tracks"}));
-  const savedTracks = await getSavedTracks(api);
-  sendMessage(JSON.stringify({state: "playlist-tracks"}));
-  const playlistTracks = await getTracksFromPlaylists(api, userPlaylists.map(p => p.id));
-  const tracks = playlistTracks.concat(savedTracks);
-  sendMessage(JSON.stringify({state: "audio-features"}));
-  const audioFeatures = await getTrackFeatures(api, tracks.map(p => p.id));
-  const tracksWithAudioFeatures = mergeTracksWithFeatures(tracks, audioFeatures);
-  sendMessage(JSON.stringify({state: "done"}));
+  // add liked songs playlist
+  userPlaylists.push({
+      "collaborative": false,
+      "id": user.id,
+      "name": "Liked Songs",
+      "owner": user,
+      "public": false,
+      "type": "playlist",
+  });
+
+  messageCallback(codes.USER_SAVED_TRACKS);
+  const savedPlaylistItems = await getSavedItems(api, user.id);
+
+  messageCallback(codes.PLAYLIST_TRACKS);
+  const playlistItems = await getPlaylistItemsFromPlaylists(api, userPlaylists.map(p => p.id));
+  const allPlaylistItems = playlistItems.concat(savedPlaylistItems);
+
+  // add all the tracks to their playlists
+  putTrackIdsIntoPlaylist(userPlaylists, allPlaylistItems);
+  // prepare for export
+  const allTracks = removeDuplicatesFromTracks(convertPlaylistItemsToTracks(allPlaylistItems));
+
+  messageCallback(codes.AUDIO_FEATURES);
+  const audioFeatures = await getTrackFeatures(api, allTracks.map(p => p.id));
+  const tracksWithAudioFeatures = mergeTracksWithFeatures(allTracks, audioFeatures);
+  
   return {
     user,
     playlists: userPlaylists,
@@ -43,7 +72,7 @@ async function getSpotifyInfo(api, sendMessage) {
 /***************************Helper functions****************************/
 // Converts a list of objects to a dictionary by IDs
 function objectListToDictById(objects) {
-  return objects.reduce((acc, o) => (acc[o.id] = o, acc), {});
+  return objects.filter(o=>o && o.id != null).reduce((acc, o) => (acc[o.id] = o, acc), {});
 }
 
 // Takes all duplicates out of tracks
@@ -55,7 +84,7 @@ function removeDuplicatesFromTracks(tracks) {
 // Because one object in the cloud without joins is easier
 function mergeTracksWithFeatures(tracks, audioFeatures) {
   const trackDict = objectListToDictById(tracks);
-  return audioFeatures.map(audioFeature => {
+  return audioFeatures.filter(af=>af!=null).map(audioFeature => {
     return {
       ...audioFeature,
       ...trackDict[audioFeature.id],
@@ -77,6 +106,33 @@ async function asyncMap(values, func) {
   }
   return acc.reverse();
 }
+
+function convertPlaylistItemsToTracks(playlistTracks) {
+  return playlistTracks
+    .map(pt => pt?.track)
+    .filter(t => t != null)
+    .filter(t => t.id != null)
+    .filter(t => t.is_local == false);
+}
+
+function putTrackIdsIntoPlaylist(playlists, playlistTracks) {
+  const playlistsDict = objectListToDictById(playlists);
+  playlists.forEach(p=>{
+    p.tracks = [];
+  })
+  playlistTracks.filter(pt=>pt!=null).forEach(pt => {
+    const pid = pt.playlistId;
+    if (pid in playlistsDict) {
+      playlistsDict[pid].tracks.push({
+        id: pt.track.id,
+        added_at: pt.added_at
+      });
+    } else {
+      console.log("could not find playlist id");
+    }
+  });
+}
+
 /***********************************************************************/
 
 
@@ -87,6 +143,9 @@ async function getUserInfo(api) {
     return data.body;
   } catch (err) {
     console.log('Something went wrong!', err);
+    if (err.statusCode == 401) {
+      // TODO REPORT
+    }
     return undefined;
   }
 }
@@ -135,11 +194,15 @@ async function getNextUntilDone(func, listKeyName, endOffset) {
 
 
 // Requests all of the saved tracks for a user
-async function getSavedTracks(api) {
-  return await getNextUntilDone(offset => api.getMySavedTracks({
+async function getSavedItems(api, playlistId) {
+  const savedPlaylistItems = await getNextUntilDone(offset => api.getMySavedTracks({
     offset: offset * 50,
     limit: 50
   }));
+  return savedPlaylistItems.map(i => {
+    i.playlistId = playlistId;
+    return i;
+  });
 }
 
 
@@ -153,25 +216,26 @@ async function getUserPlaylists(api, username) {
 }
 
 // processes all playlists, filter to get rid of the nulls, sends to getPlaylistTracks
-async function getTracksFromPlaylists(api, playlistIds) {
-  const playlistArrays = await Promise.all(playlistIds.map(id => getPlaylistTracks(api, id)));
-  let playlistTracks = [];
+async function getPlaylistItemsFromPlaylists(api, playlistIds) {
+  const playlistArrays = await Promise.all(playlistIds.map(id => getPlaylistItems(api, id)));
+  let playlistItems = [];
   for (let playlistArray of playlistArrays) {
-    playlistTracks = playlistTracks.concat(playlistArray);
+    playlistItems = playlistItems.concat(playlistArray);
   }
-  const tracks = playlistTracks
-    .map(pt => pt?.track)
-    .filter(t => t != null)
-    .filter(t => t.is_local == false);
-  return removeDuplicatesFromTracks(tracks);
+  return playlistItems;
 }
 
 // gets all of the tracks associated with an individual playlist
-async function getPlaylistTracks(api, playlistId) {
-  return await getNextUntilDone(offset => api.getPlaylistTracks(playlistId, {
+async function getPlaylistItems(api, playlistId) {
+  const playlistItems = await getNextUntilDone(offset => api.getPlaylistTracks(playlistId, {
     offset: offset * 50,
     limit: 50
   }));
+  playlistItems.forEach(pi => {
+    pi.playlistId = playlistId;
+    return pi;
+  });
+  return playlistItems;
 }
 /***********************************************************************/
 
@@ -184,9 +248,6 @@ async function getTrackFeatures(api, trackIds) {
     Math.ceil(trackIds.length / APIlimit)
   );
 }
-
-
-
 
 
 /***********************************************************************
@@ -210,16 +271,69 @@ app.get('/sync', async (req, res) => {
     clientId: my_client_id,
     redirectUri: 'http://localhost:3000/callback'
   });
-  // starts syncing the spotify data into firebase
   const token = req.query.access_token;
   if (token) {
     // Get the authenticated user
     api.setAccessToken(token);
+    
     const id = uuid.v4();
     const wsapi = WebSocketAPI(id);
+    // TODO: check if stuff is already in firebase
     getSpotifyInfo(api, wsapi.messageCallback).then((data) => {
-      wsapi.messageCallback(JSON.stringify(data));
-    })
+      // send data to the browser
+      wsapi.messageCallback(data);
+
+      /***********************************************************************
+      * Send data to firebase
+      ***********************************************************************/
+      
+      // Create the user in database
+      wsapi.messageCallback(LOADING_CODE.UPLOAD_USER_INFO);
+      const userDocRef = db.collection('users').doc(data.user.id);
+      userDocRef.set(data.user)
+        .then(function () {
+          console.log("Added a User");
+        })
+        .catch(error => {
+          console.log("An error occured: ", error);
+        });
+        
+
+      // Helper function to gather lists of playlists/tracks into batches
+      // of 500 and add to database (Spotify API limits to calls of 500 every second) 
+      async function limitBatchTo500(colName, items) {
+        let count = 0;
+
+        while (count < items.length) {
+          currentBatchItems = items.slice(count, count + 500);
+          
+          // Create the user's playlists in database
+          wsapi.messageCallback(LOADING_CODE.USER_PLAYLISTS);
+          let batch = db.batch();
+          currentBatchItems.forEach((item) => {
+            const docRef = db.collection(colName).doc(item.id)
+            batch.set(docRef, item);
+          });
+          await batch.commit();
+
+          count += currentBatchItems.length;
+        }
+      }
+
+      // Create the user's playlists
+      wsapi.messageCallback(LOADING_CODE.UPLOAD_PLAYLISTS);
+      limitBatchTo500("playlists", data.playlists);
+
+      // Create the user's tracks in database
+      wsapi.messageCallback(LOADING_CODE.UPLOAD_TRACKS);
+      limitBatchTo500("tracks", data.tracks);
+
+      // after uploaded
+      wsapi.messageCallback(LOADING_CODE.DONE);
+    }).catch((error)=>{
+      console.log(error);
+      wsapi.messageCallback(STATUS_CODE.INVALID_TOKEN);
+    });
     
     fs.readFile(path.join('private', 'login-success.html'), function (err, data) {
       if (err) {
@@ -247,8 +361,33 @@ app.get('/sync', async (req, res) => {
 
 const connections = {};
 
+const LOADING_CODE = {
+  INITIALIZE: "",
+  USER_INFO: "",
+  USER_PLAYLISTS: "",
+  USER_SAVED_TRACKS: "",
+  PLAYLIST_TRACKS: "",
+  AUDIO_FEATURES: "",
+  UPLOAD_USER_INFO: "",
+  UPLOAD_PLAYLISTS: "",
+  UPLOAD_TRACKS: "",
+  DONE: "",
+};
+const STATUS_CODE = {
+  INVALID_TOKEN: {error: 'invalid-token', message: 'please get new token'}
+}
+// build LOADING_CODE
+Object.keys(LOADING_CODE).forEach((v,i) => {
+  LOADING_CODE[v] = {
+    name: v,
+    index: i,
+    total: Object.keys(LOADING_CODE).length
+  };
+});
+console.log(LOADING_CODE)
+
 function WebSocketAPI(id) {
-  const messageCallback = (message) => {
+  const messageCallback = (data) => {
     if (!(id in connections)) {
       connections[id] = {
         queue: [],
@@ -258,15 +397,13 @@ function WebSocketAPI(id) {
     }
     
     const connection = connections[id];
-    if (message != undefined) {
-      connection.queue.push(message);
+    if (data != undefined) {
+      connection.queue.push(JSON.stringify(data));
     }
     if (connection.sendWSMessage !== undefined) {
       while (connection.queue.length) {
         connection.sendWSMessage(connection.queue.shift());
       }
-    } else {
-      console.log(connection, connections[id])
     }
   }
 
@@ -274,16 +411,16 @@ function WebSocketAPI(id) {
 }
 
 
-app.ws('/echo', function (ws, req) {
+app.ws('/progress', function (ws, req) {
   ws.on('message', function (id) {
     console.log(id);
     if (id in connections) {
       connections[id].sendWSMessage = (message) => {
         ws.send(message);
       }
-      console.log(connections);
     } else {
       console.log("bad connection id");
+      ws.send(JSON.stringify({error: "bad connection id"}));
     }
   });
 });
